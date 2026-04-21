10f1f708409a58a956de86e93e522b5569a02623
JuanArrua208 <108731993+JuanArrua208@users.noreply.github.com>
2026-04-21 11:51:54 -0600

    Stabilize frontend for Vercel deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './elements/Home'
-import Create from './elements/Create'
-import Edit from './elements/Edit'
-import Read from './elements/Read'
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./elements/Home";
+import Create from "./elements/Create";
+import Edit from "./elements/Edit";
+import Read from "./elements/Read";
 
-function App(){
+function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/create' element={<Create />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='/read/:id' element={<Read />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/read/:id" element={<Read />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
