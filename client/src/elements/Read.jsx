a1c16924e4a32e3a6894367dc0202d64b7c11df4
JuanArrua208 <108731993+JuanArrua208@users.noreply.github.com>
2026-04-21 13:57:03 -0600

    Fix Vercel build and clean create form texts

--- client/src/elements/Read.jsx
+++ client/src/elements/Read.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import api from "../api";
 import { formatSalary } from "../utils";
